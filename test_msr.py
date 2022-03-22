003505cb7c428e9e004740f2518a598e2ea59c1e
yongzhang <62042784+yongzhangwhu@users.noreply.github.com>
2022-03-22 10:19:40 +0800

    Update test_msr.py

--- test_msr.py
+++ test_msr.py
@@ -1,24 +1,19 @@
 import os
+import cv2
 import argparse
 import shutil
 import importlib
 import numpy as np
-import time
 
 import torch
 from torch.utils.data import DataLoader
-import cv2
-from tqdm import tqdm
 
 from datasets.load_dataset import LoadMSR_dataset_test
 from model.common import print_model_parm_nums
-import torch.nn as nn
 from TorchTools.ArgsTools.test_args import TestArgs
 from TorchTools.LogTools.logger_tensorboard import Tf_Logger
-from TorchTools.LossTools.loss import C_Loss, VGGLoss
-from TorchTools.LossTools.metrics import PSNR, AverageMeter
-import utils  
-
+from TorchTools.LossTools.metrics import AverageMeter
+import utils
 
 def main():
     ###############################################################################################
